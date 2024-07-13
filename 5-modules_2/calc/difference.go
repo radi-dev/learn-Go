5b751cac66122d8a016096daf2642d055c1b7081
Radi <evaradi18@gmail.com>
2024-07-13 07:59:11 +0100

    modules(2)

---
+++ 5-modules_2/calc/difference.go
@@ -0,0 +1,9 @@
+package calc
+
+import (
+	"math"
+)
+
+func Difference(a, b int) (float64,int) {
+	return math.Abs(float64(a - b)),a - b
+}
---
+++ 5-modules_2/modules.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"./calc/difference"
+)
+
+func main() {
+	fmt.Println(difference.Difference(7, 40))
+}
+
+//cd 5-modules_2
+//go mod init modules
+//go run .
