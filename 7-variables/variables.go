bb50ca0d31107dcdfea1c1c5aaad237c928f0a5d
Radi <evaradi18@gmail.com>
2024-07-14 16:41:14 +0100

    more vars

--- 7-variables/variables.go
+++ 7-variables/variables.go
@@ -6,6 +6,10 @@ var declared_var_1 int
 var declared_var_2, declared_var_3 int
 var initialized_var_w_type int = 3
 var initialized_var_no_type = 3
+var (
+	bracketed_var_1 = 5
+	bracketed_var_2 = 6
+)
 
 //---short forms only work inside functions
 // bad_short_form_initialized_var := 2
@@ -16,11 +20,11 @@ func main() {
 	declared_var_2 = 4
 
 	short_form_initialized_var := 2
-	short_form_initialized_vars_1, short_form_initialized_vars_2 := 2,8
+	short_form_initialized_vars_1, short_form_initialized_vars_2 := 2, 8
 
 	fmt.Println(
-		short_form_initialized_var +
-		short_form_initialized_vars_1 +
-		short_form_initialized_vars_2)
+		short_form_initialized_var, '\n',
+		short_form_initialized_vars_1, '\n',
+		short_form_initialized_vars_2, '\n', bracketed_var_2)
 
 }
