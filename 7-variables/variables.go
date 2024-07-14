1ecfe7b1368a07a6a20d1f7f948ce5d5ec433eed
Radi <evaradi18@gmail.com>
2024-07-14 15:49:23 +0100

    variables

---
+++ 7-variables/variables.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+var declared_var_1 int
+var declared_var_2, declared_var_3 int
+var initialized_var_w_type int = 3
+var initialized_var_no_type = 3
+
+//---short forms only work inside functions
+// bad_short_form_initialized_var := 2
+// bad_short_form_initialized_vars_1, bad_short_form_initialized_vars_2 := 2
+
+func main() {
+	declared_var_1 = 2
+	declared_var_2 = 4
+
+	short_form_initialized_var := 2
+	short_form_initialized_vars_1, short_form_initialized_vars_2 := 2,8
+
+	fmt.Println(
+		short_form_initialized_var +
+		short_form_initialized_vars_1 +
+		short_form_initialized_vars_2)
+
+}
