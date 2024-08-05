edb430b4258460a96069f0e40c6e6475774c9de1
Radi <evaradi18@gmail.com>
2024-08-05 05:26:17 +0100

    arrays

---
+++ 15-arrays/arrays.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var arr [2]string
+	arr[0] = "Ra"
+	arr[1] = "di"
+	fmt.Println(arr[0] + arr[1])
+	fmt.Println(arr)
+
+	blood_code := [5]int{2, 12, 15, 15, 4}
+	fmt.Println(blood_code)
+}
+
+//cd 15-arrays
+//go run arrays.go
+
+/*
+- Cannot be resized
+- An array's length is part of its type
+- The type [n]T is an array of n values of type T
+*/
